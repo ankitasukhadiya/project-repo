19550db61dfc8dec541eb520860d2ebe405dad24
ankitasukhadiya <ankitas.tagline@gmail.com>
2022-09-20 16:21:21 +0530

    car demo

--- django_application/admin.py
+++ django_application/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.conf import settings
-
 from .models import Car,User,BuyCar
 
 admin.site.register(Car)
--- django_application/views.py
+++ django_application/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Car
+from .models import Car,BuyCar,User
 from .forms import CarForm
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 def home(request):
@@ -13,28 +14,38 @@ def base(request):
     return render(request,'base.html') 
 
 def thankyou(request):
-    return render(request,'thankyou.html') 
+    data = Car.objects.all().values()
+    context = {
+        'data' : data,
+    }
+    return render(request,'thankyou.html',context) 
     
-def carlist(request):
-    # return render(request,'carlist.html') 
+def carlist(request):     
     form = CarForm 
     if 'save' in request.POST :
-        data = CarForm(request.POST, request.FILES)
+        data = CarForm(request.POST)
         if data.is_valid():
             data.save()
             return redirect('django_application:thankyou')
     return render(request , 'carlist.html',{'form':form})   
 
-def findcar(request):
-    return render(request,'findcar.html')
-    # data = Car.objects.all()   
-    # paginator = Paginator(data, 5) 
-    # page_number = request.GET.get('page')
-    # data = paginator.get_page(page_number)
-    # context = {
-    #         'data': data,
-    #     }     
-    # return render(request, "findcar.html", context)
+def findcar(request):     
+    data = Car.objects.all()
+    searchdata = request.GET.get('search')
+    if searchdata is not None: 
+        data = data.filter(Q(make__icontains = searchdata) | Q(year__icontains = searchdata)).distinct()           
+    paginator = Paginator(data,10) 
+    page_number = request.GET.get('page')
+    data = paginator.get_page(page_number)
+    context = {
+        'data' : data
+    }
+    return render(request,'findcar.html',context)
+
+   
+     
+    
+        
 
 
 
